bafea20ee5a6c35b285ff1bcbe0a96e442930330
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2024-05-03 18:00:11 +0300

    Change temp h2 DB to permanent mysql on my localhost

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS productTable (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    description varchar(50) NOT NULL,
-    price INT NOT NULL,
-    stock BOOLEAN NOT NULL
-);
