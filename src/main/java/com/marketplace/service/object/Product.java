8973b8511986bd815c6313a4e630d6ba5b208cc8
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-10-06 14:19:31 +0300

    Updated project with Lombok

--- src/main/java/com/marketplace/service/object/Product.java
+++ src/main/java/com/marketplace/service/object/Product.java
@@ -1,5 +1,10 @@
 package com.marketplace.service.object;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Product {
     
     // Описание товара.
@@ -8,45 +13,5 @@ public class Product {
     private String description;
     private int price;
     private boolean stock;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setStock(boolean stock) {
-        this.stock = stock;
-    }
-
-    public boolean getStock() {
-        return stock;
-    }
 
 }
--- src/main/java/com/marketplace/service/repository/ProductRepository.java
+++ src/main/java/com/marketplace/service/repository/ProductRepository.java
@@ -20,46 +20,26 @@ public class ProductRepository {
     // Сохранение нового товара.
     public Product save(Product product) {
         String sql = "INSERT INTO productTable (name, description, price, stock) VALUES (?, ?, ?, ?)";
-        jdbc.update(sql, product.getName(), product.getDescription(), product.getPrice(), product.getStock());
+        jdbc.update(sql, product.getName(), product.getDescription(), product.getPrice(), product.isStock());
         return product;
     }
 
     // Вывод всех товаров.
     public List<Product> findAll() {
         String sql = "SELECT * FROM productTable";
-        RowMapper<Product> productRowMapper = (r, i) -> {
-            Product rowObject = new Product();
-            rowObject.setId(r.getInt("id"));
-            rowObject.setName(r.getString("name"));
-            rowObject.setDescription(r.getString("description"));
-            rowObject.setPrice(r.getInt("price"));
-            rowObject.setStock(r.getBoolean("stock"));
-            return rowObject;
-        };
-
         return jdbc.query(sql, productRowMapper);
     }
 
     // Поиск товара по ID.
     public Product findById(int id) {
         String sql = "SELECT * FROM productTable WHERE id = ?";
-        RowMapper<Product> productRowMapper = (r, i) -> {
-            Product rowObject = new Product();
-            rowObject.setId(r.getInt("id"));
-            rowObject.setName(r.getString("name"));
-            rowObject.setDescription(r.getString("description"));
-            rowObject.setPrice(r.getInt("price"));
-            rowObject.setStock(r.getBoolean("stock"));
-            return rowObject;
-        };
-
         return jdbc.queryForObject(sql, productRowMapper, id);
     }
 
     // Обновление продукта.
     public Product updateById(Product product, int id) {
         String sql = "UPDATE productTable SET name = ?, description = ?, price = ?, stock = ? WHERE id = ?";
-        jdbc.update(sql, product.getName(), product.getDescription(), product.getPrice(), product.getStock(), id);
+        jdbc.update(sql, product.getName(), product.getDescription(), product.getPrice(), product.isStock(), id);
         return product;
     }
 
@@ -68,5 +48,15 @@ public class ProductRepository {
         String sql = "DELETE FROM productTable WHERE id=?";
         jdbc.update(sql, id);
     }
+
+    RowMapper<Product> productRowMapper = (r, i) -> {
+        Product rowObject = new Product();
+        rowObject.setId(r.getInt("id"));
+        rowObject.setName(r.getString("name"));
+        rowObject.setDescription(r.getString("description"));
+        rowObject.setPrice(r.getInt("price"));
+        rowObject.setStock(r.getBoolean("stock"));
+        return rowObject;
+    };
     
 }
